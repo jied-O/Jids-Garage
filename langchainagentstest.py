fd0bc5bf5031064cbdce9af44d7284420fe930f2
jied-O <jide@ogbuji.net>
2024-02-04 18:18:46 -0700

    no idea what's going on here ngl

--- langchainagentstest.py
+++ langchainagentstest.py
@@ -11,21 +11,21 @@ from ogbujipt.model_style.alpaca import prep_instru_inputs, ALPACA_PROMPT_TMPL
 from langchain.prompts import PromptTemplate
 openai_emulation(host="http://192.168.0.73", port="8000")
 
-def simpleWordPrompt():
+def name_the_capital(country: str="Nigeria", temperature: float=0.1):
     prompt = PromptTemplate(
         input_variables=["place"],
         template="What is the capital of {place}?",
     )
-    print(prompt.format(place="Nigeria"))
+    print(prompt.format(place=country))
 
-    llm = OpenAI(temperature=0.1)
+    llm = OpenAI(temperature=temperature)
     llmchain = LLMChain(llm=llm, prompt=prompt)
-    response = llmchain.run(place="Nigeria")
+    response = llmchain.run(place=country)
     print(response)
 
 
-def MathWorldProblem():
-    llm = OpenAI(temperature=0.1)
+def math_world_problem(temperature: float=0.1):
+    llm = OpenAI(temperature=temperature)
     palchain = PALChain.from_math_prompt(llm=llm, verbose=True)
     response = palchain.run(
         "If my age is half of my dad's age and he is going to be 60 next year, what is my current age?"
@@ -33,8 +33,8 @@ def MathWorldProblem():
     print(response)
 
 
-def agentTest():
-    llm = OpenAI(temperature=0)
+def agent_test(temperature: float=0):
+    llm = OpenAI(temperature=temperature)
     tools = load_tools(["pal-math"], llm=llm)
     agent = initialize_agent(tools,
                          llm,
@@ -44,8 +44,10 @@ def agentTest():
 
 
 def main():
-    MathWorldProblem()
+    name_the_capital("France")
 
 
 if __name__ == "__main__":
     main()
+
+    #wow i love duckly!!!!!!!!!!!!!
